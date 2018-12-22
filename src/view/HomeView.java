2c32e69feaea530724943e12111b237c837db47b
vvergarahack <veronica.vergara@njwg.cap.gov>
2018-12-21 21:07:25 -0500

    Check-in #1

--- src/view/HomeView.java
+++ src/view/HomeView.java
@@ -4,7 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
-
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 import controller.ViewManager;
@@ -13,7 +13,14 @@ import controller.ViewManager;
 public class HomeView extends JPanel implements ActionListener {
 	
 	private ViewManager manager;		// manages interactions between the views, model, and database
-	
+	private JButton logOffButton;
+	private JButton depositButton;
+	private JButton withdrawButton;
+	private JButton transferButton;
+	//private JButton viewBalanceButton;
+	//private JButton viewAccountButton;
+	//private JButton editAccountButton;
+	//private JButton closeAccountButton;
 	/**
 	 * Constructs an instance (or objects) of the HomeView class.
 	 * 
@@ -41,7 +48,14 @@ public class HomeView extends JPanel implements ActionListener {
 		// building the HomeView.
 		
 		this.add(new javax.swing.JLabel("HomeView", javax.swing.SwingConstants.CENTER));
-		
+		initLogOffButton();
+		initDepositButton();
+		initWithdrawButton();
+		initTransferButton();
+//		initViewBalanceButton();
+//		initViewAcctButton();
+//		initEditAcctButton();
+//		initCloseAcctButton();
 		// TODO
 		//
 		// this is where you should build the HomeView (i.e., all the components that
@@ -58,6 +72,38 @@ public class HomeView extends JPanel implements ActionListener {
 	 * @throws IOException
 	 */
 	
+	private void initLogOffButton() {	
+		logOffButton = new JButton("Log Out");
+		logOffButton.setBounds(150, 100, 200, 35);
+		logOffButton.addActionListener(this);
+		
+		this.add(logOffButton);
+	}
+	
+	private void initDepositButton() {	
+		depositButton = new JButton("Deposit");
+		depositButton.setBounds(150, 150, 200, 35);
+		depositButton.addActionListener(this);
+		
+		this.add(depositButton);
+	}
+	
+	private void initWithdrawButton() {
+		withdrawButton = new JButton("Withdraw");
+		withdrawButton.setBounds(150, 200, 200, 35);
+		withdrawButton.addActionListener(this);
+		
+		this.add(withdrawButton);
+	}
+	
+	private void initTransferButton() {
+		transferButton = new JButton("Transfer");
+		transferButton.setBounds(150, 250, 200, 35);
+		transferButton.addActionListener(this);
+		
+		this.add(transferButton);
+	}
+	
 	private void writeObject(ObjectOutputStream oos) throws IOException {
 		throw new IOException("ERROR: The HomeView class is not serializable.");
 	}
@@ -72,7 +118,24 @@ public class HomeView extends JPanel implements ActionListener {
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		Object source = e.getSource();
 		
+		if(source.equals(logOffButton)) {
+			manager.login(null, null);
+			manager.switchTo(ATM.LOGIN_VIEW);
+		}
+		else if(source.equals(depositButton)) {
+			manager.switchTo(ATM.DEPOSIT_VIEW);
+		}
+		else if(source.equals(withdrawButton)) {
+			manager.switchTo(ATM.WITHDRAW_VIEW);
+		}
+		else if(source.equals(transferButton)) {
+			manager.switchTo(ATM.TRANSFER_VIEW);
+		}
+		else {
+			System.err.println("ERROR: Action command not found (" + e.getActionCommand() + ")");
+		}
 		// TODO
 		//
 		// this is where you'll setup your action listener, which is responsible for
