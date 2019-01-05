c7d39b35875fb2151bc70ea602fb4a091476bc2e
vvergarahack <veronica.vergara@njwg.cap.gov>
2019-01-04 19:11:44 -0500

    Check-in #2

--- src/view/LoginView.java
+++ src/view/LoginView.java
@@ -2,9 +2,12 @@ package view;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
@@ -86,6 +89,7 @@ public class LoginView extends JPanel implements ActionListener {
 		
 		accountField = new JTextField(20);
 		accountField.setBounds(205, 100, 200, 35);
+		accountField.addActionListener(this);
 		
 		this.add(label);
 		this.add(accountField);
@@ -103,6 +107,7 @@ public class LoginView extends JPanel implements ActionListener {
 		
 		pinField = new JPasswordField(20);
 		pinField.setBounds(205, 140, 200, 35);
+		pinField.addActionListener(this);
 		
 		this.add(label);
 		this.add(pinField);
