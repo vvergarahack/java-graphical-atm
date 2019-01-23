5bbc3f8a9c0714e37befbaf61a46edbf2b8c54ce
vvergarahack <veronica.vergara@njwg.cap.gov>
2019-01-22 22:07:22 -0500

    Update

--- src/view/WithdrawView.java
+++ src/view/WithdrawView.java
@@ -2,20 +2,20 @@ package view;
 
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.Image;
+//import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
+//import java.io.File;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
+//import javax.imageio.ImageIO;
+//import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
+//import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JPasswordField;
+//import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
